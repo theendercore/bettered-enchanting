afc19bdde368092d37e40760d651a026c1b98c73
TheEnderCore <theendercore@gmail.com>
2025-09-06 21:37:44 +0300

    will deal with this latter

--- src/main/java/cutefox/bettered_enchanting/init/BELootTableModifiers.java
+++ src/main/java/cutefox/bettered_enchanting/init/BELootTableModifiers.java
@@ -2,6 +2,7 @@ package cutefox.bettered_enchanting.init;
 
 import cutefox.bettered_enchanting.BetteredEnchanting;
 import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
+import net.minecraft.loot.LootPool;
 import net.minecraft.loot.LootTables;
 import net.minecraft.loot.entry.ItemEntry;
 
@@ -12,12 +13,14 @@ public class BELootTableModifiers {
         BetteredEnchanting.LOGGER.info("Modifying loot tables for : " + BetteredEnchanting.MODID);
 
         LootTableEvents.MODIFY.register((key, builder, source, registries) -> {
-            if (source.isBuiltin() && LootTables.PIGLIN_BARTERING_GAMEPLAY == key) {
-
+            if (LootTables.PIGLIN_BARTERING_GAMEPLAY == key) {
                 builder.modifyPools(p -> {
                     p.with(ItemEntry.builder(BEItems.SOUL_ESSENCE).weight(15));
                 });
             }
+//            if (LootTables.ANCIENT_CITY_CHEST == key) {
+//                builder.pool(LootPool.builder().with(ItemEntry.builder(BEItems.ESSENCE_OF_SNEAK).weight(0)).build());
+//            }
         });
 
         LootTableEvents.MODIFY.register((key, builder, source, registries) -> {
